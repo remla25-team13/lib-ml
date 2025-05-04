a41cbf7c2bf8d85353f02813903f2307a1118c99
Tijmen Meijer <49029340+surftijmen@users.noreply.github.com>
2025-05-04 18:34:47 +0200

    Setup lib-ml

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lib-ml',
+    description='Shared preprocessing logic',
+    author='remla25-team13',
+    packages=find_packages(),
+    install_requires=[
+        "numpy==1.26.4",
+        "scikit-learn==1.4.2",
+        "pandas==2.2.2",
+        "scipy==1.13"
+    ],
+    python_requires='>=3.10',
+)
